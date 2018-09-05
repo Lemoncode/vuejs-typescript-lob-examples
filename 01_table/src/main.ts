70d5b954ff2f745edbf2d707a04ae9b883914000
Braulio <braulio.diez@gmail.com>
2018-09-05 18:32:15 +0200

    table implementation

--- 01_table/src/main.ts
+++ 01_table/src/main.ts
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import Vuetify from 'vuetify';
+
+// tslint:disable-next-line:no-var-requires
+require('../node_modules/vuetify/dist/vuetify.min.css');
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
 new Vue({
   router,
