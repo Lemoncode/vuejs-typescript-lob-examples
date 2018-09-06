976d8b714b4c6d4e411e56e974ddc0eafcb853fc
Braulio <braulio.diez@gmail.com>
2018-09-06 10:51:29 +0200

    api start sample copied

---
+++ 02_rest_api/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template type="ts">
+  <div class="home">
+    <h3>Hello from home page</h3>
+    <UsersTable/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import UsersTable from '@/components/users-table.vue';
+
+@Component({
+  components: {
+    UsersTable,
+  },
+})
+export default class Home extends Vue {}
+</script>
