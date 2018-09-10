ab631e347f9fe133776a7f732b7ce71fd759e9ed
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-09-10 08:43:30 +0100

    Update as style guide Vue

--- 01_table/src/views/Home.vue
+++ 01_table/src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
     <h3>Hello from home page</h3>
-    <UsersTable/>
+    <users-table/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import UsersTable from '@/components/users-table.vue';
+import UsersTable from '@/components/UsersTable.vue';
 
 @Component({
   components: {
